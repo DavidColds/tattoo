d297e8f2aae25017cdb60f19b14a6321e4687e6c
David Drughi <drughidavid6@gmail.com>
2020-04-17 13:53:50 +0200

    added the rest of the pages and routed them

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='base-home'),
     path('about/', views.about, name='base-about'),
+    path('contact/', views.contact, name='base-contact'),
+    path('art/', views.art, name='base-art'),
+    path('portfolio/', views.portfolio, name='base-portfolio'),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -5,3 +5,12 @@ def home(request):
 
 def about(request):
     return render(request, 'base/about.html', {'title': 'About'})
+
+def contact(request):
+    return render(request, 'base/contact.html', {'title': 'Contact'})
+
+def art(request):
+    return render(request, 'base/art.html', {'title': 'Art'})
+
+def portfolio(request):
+    return render(request, 'base/portfolio.html', {'title': 'Portfolio'})
